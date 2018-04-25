da23ed31212b7ecb8cc0377ac32af17034b60793
patarapolw <patarapolw@gmail.com>
2018-04-25 16:53:20 +0700

    Normalize complexity with 'password' and rely on common word list

--- analysis/analysis_pronounceable.py
+++ analysis/analysis_pronounceable.py
@@ -13,8 +13,8 @@ def complexity_initial(length=10):
     """
 
     :return:
-    length=10: Best: 0.40, 0.40, 0.40; Worst: 13.40, 15.20, 16.20
-    length=15: Best: 3.13, 3.60, 3.67; Worst: 18.87, 18.93, 20.00
+    length=10: Best: 1.78, 1.85, 1.91; Worst: 4.90, 5.52, 5.86
+    length=15: Best: 2.24, 2.30, 2.31; Worst: 5.41, 5.79, 6.03
     """
     password = gp.new_initial_password(min_length=length)[0]
     if password is not None:
@@ -27,7 +27,7 @@ def complexity_diceware_policy_conformized(number_of_words=4):
     """
 
     :return:
-    number_of_words=4: Best: 14.75, 14.80, 14.91; Worst: 30.85, 32.04, 42.59
+    number_of_words=4: Best: 3.71, 3.78, 3.83; Worst: 6.51, 6.60, 6.65
     """
     password = gp.new_diceware_password(number_of_words=number_of_words)[0]
     if password is not None:
@@ -40,8 +40,8 @@ def complexity_common_diceware_policy_conformized(number_of_words=6):
     """
 
     :return:
-    number_of_words=4: Best: 8.48, 9.80, 11.00; Worst: 28.70, 29.43, 30.00
-    number_of_words=6: Best: 14.20, 16.33, 16.75; Worst: 36.95, 38.61, 41.93
+    number_of_words=4: Best: 2.57, 3.01, 3.18; Worst: 5.97, 6.62, 6.76
+    number_of_words=6: Best: 4.08, 4.24, 4.42; Worst: 7.32, 7.97, 8.24
     """
     password = gp.new_common_diceware_password(number_of_words=number_of_words)[0]
     if password is not None:
@@ -50,13 +50,13 @@ def complexity_common_diceware_policy_conformized(number_of_words=6):
         return False
 
 
-def complexity_common_diceware_all_lower(number_of_words=6):
+def complexity_common_diceware_all_lower(number_of_words=4):
     """
 
     :param number_of_words:
     :return:
-    number_of_words=4: Best: 2.50, 4.09, 4.26; Worst: 11.77, 12.75, 12.80
-    number_of_words=6: Best: 8.73, 8.77, 8.83; Worst: 20.49, 21.50, 22.48
+    number_of_words=4: Best: 2.15, 2.15, 2.21; Worst: 4.09, 4.13, 4.14
+    number_of_words=6: Best: 2.37, 3.35, 3.37; Worst: 5.61, 6.04, 6.61
     """
     password = ''.join([word_tool.get_random_common_word() for _ in range(number_of_words)])
     return complexity.complexity(password)
@@ -67,8 +67,8 @@ def complexity_pronounceable_word_all_lower(number_of_words=6):
 
     :param number_of_words:
     :return:
-    number_of_words=4: Best: -1.82, -1.00, -0.82; Worst: 4.50, 5.26, 6.20
-    number_of_words=6: Best: 2.04, 2.09, 2.14; Worst: 9.80, 10.86, 10.89
+    number_of_words=4: Best: 1.12, 1.13, 1.13; Worst: 2.34, 2.65, 2.73
+    number_of_words=6: Best: 1.53, 1.67, 1.69; Worst: 3.13, 3.17, 3.79
     """
     password = ''.join([generate_word() for _ in range(number_of_words)])
     return complexity.complexity(password)
@@ -79,8 +79,8 @@ def complexity_pronounceable_length_all_lower(min_length=20):
 
     :param min_length:
     :return:
-    min_length=15: Worst: Best: -0.74, -0.59, -0.40; Worst: 4.60, 5.33, 5.33
-    min_length=20: Worst: Best: 1.09, 1.20, 2.09; Worst: 8.00, 8.04, 8.09
+    min_length=15: Best: 1.00, 1.06, 1.09; Worst: 2.40, 2.43, 2.72
+    min_length=20: Best: 1.52, 1.69, 1.69; Worst: 2.88, 3.07, 3.17
     """
     password = pw.length(min_length, min_length+5)
     return complexity.complexity(password)
@@ -104,4 +104,4 @@ if __name__ == '__main__':
     # timeit(test_initial_entropy, validator=lambda x: x)
     from functools import partial
 
-    check_complexity(complexity_initial)
+    check_complexity(complexity_common_diceware_policy_conformized)
